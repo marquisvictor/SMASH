d5be94d04d74e004629fff06eda10bb94101f1fa
mtralka <mtralka@umd.edu>
2021-09-13 11:34:15 -0400

    SMAART page centering

--- SMASH/pages/smaart.vue
+++ SMASH/pages/smaart.vue
@@ -4,29 +4,19 @@
       title="SMAART"
       subtitle="Spatial Multiscale Analytics, Applied Research, and Technology Seminar"
     >
-      <div class="flex flex-col items-center">
-        <!-- <p
-          class="max-w-3xl mx-5 mt-4 text-2xl font-medium tracking-wide text-justify md:mx-0"
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Erat
-          imperdiet sed euismod nisi porta lorem mollis aliquam ut. Et magnis
-          dis parturient montes.
-        </p> -->
-        <div
-          class="grid grid-cols-1 gap-6 p-10 mx-auto  md:w-9/12 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2"
-        >
-          <Card
-            v-for="video in smaartVideos"
-            :key="video.title"
-            :title="video.title"
-            :date="video.date"
-            :video-id="video.videoId"
-            :subtitle="video.presenter"
-            class="max-w-md m-3"
-            style="min-height: 14rem"
-          />
-        </div>
+      <div
+        class="grid grid-cols-1 gap-6 p-10 mx-auto  md:w-9/12 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 justify-items-center"
+      >
+        <Card
+          v-for="video in smaartVideos"
+          :key="video.title"
+          :title="video.title"
+          :date="video.date"
+          :video-id="video.videoId"
+          :subtitle="video.presenter"
+          class="max-w-md m-3"
+          style="min-height: 14rem"
+        />
       </div>
     </pageTitle>
   </div>
