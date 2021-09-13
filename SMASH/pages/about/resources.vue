f9ee7ede18ae22bef60708bf856c7568d9bd5d15
mtralka <mtralka@umd.edu>
2021-09-13 12:12:37 -0400

    Resource page

--- SMASH/pages/about/resources.vue
+++ SMASH/pages/about/resources.vue
@@ -3,22 +3,8 @@
     <pageTitle title="Resources">
       <p
         class="max-w-3xl mx-auto mt-4 text-2xl font-medium tracking-wide prose prose-2xl text-justify "
-      >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Erat imperdiet sed
-        euismod nisi porta lorem mollis aliquam ut. Et magnis dis parturient
-        montes. Sed vulputate odio ut enim blandit volutpat maecenas volutpat
-        blandit. At auctor urna nunc id cursus metus aliquam eleifend mi.
-        Feugiat in ante metus dictum at tempor. Aliquet eget sit amet tellus
-        cras adipiscing enim. Odio morbi quis commodo odio aenean sed. Varius
-        morbi enim nunc faucibus a pellentesque sit. Tortor consequat id porta
-        nibh venenatis. Id eu nisl nunc mi. Dignissim enim sit amet venenatis
-        urna cursus eget. Cras fermentum odio eu feugiat. Cursus turpis massa
-        tincidunt dui ut ornare lectus sit. Nunc sed id semper risus in
-        hendrerit gravida rutrum. Egestas integer eget aliquet nibh praesent
-        tristique. At volutpat diam ut venenatis tellus in metus vulputate eu.
-        Erat pellentesque adipiscing commodo elit.
-      </p>
+        v-html="$md.render(resources.body)"
+      ></p>
     </pageTitle>
   </div>
 </template>
@@ -31,5 +17,14 @@ export default {
     pageTitle,
   },
   layout: 'header-footer',
+  data() {
+    return {
+      resources: '',
+    }
+  },
+  async fetch() {
+    const resources = await this.$content('resources').fetch()
+    this.resources = resources
+  },
 }
 </script>
