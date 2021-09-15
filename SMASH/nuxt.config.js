5f8cf14fef210da2949e4ecd5287c16e5a2f2e7d
mtralka <mtralka@umd.edu>
2021-09-15 14:43:20 -0400

    add humans.txt

--- SMASH/nuxt.config.js
+++ SMASH/nuxt.config.js
@@ -38,6 +38,7 @@ export default {
     '@nuxtjs/device',
     '@nuxt/image',
     '@aceforth/nuxt-netlify',
+    '@luxdamore/nuxt-humans-txt',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -66,6 +67,35 @@ export default {
         to: '/',
         status: 302,
         force: true,
+      },
+    ],
+  },
+  humansTxt: {
+    enabled: true,
+    hideGenericMessagesInConsole: false,
+    hideErrorsInConsole: false,
+    keepDevelopersInformations: false,
+    thanksTo: [
+      {
+        key: 'Matthew Tralka',
+        value: 'Designer & Developer',
+      },
+      {
+        key: 'Undergraduate',
+        value: 'University of Maryland College Park',
+      },
+      'www.tralka.xyz',
+      false,
+    ],
+    site: [
+      'SITE',
+      {
+        key: 'Last update',
+        value: new Date().toLocaleDateString('en-US', {
+          month: '2-digit',
+          day: '2-digit',
+          year: 'numeric',
+        }),
       },
     ],
   },
