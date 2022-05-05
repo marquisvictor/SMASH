d1dea890bf5df6807a07f998efce123ec18362fc
mtralka <mtralka@umd.edu>
2022-05-05 15:45:43 -0400

    :sparkles: remove individual projects from mobile menu

--- SMASH/components/Header/Header.vue
+++ SMASH/components/Header/Header.vue
@@ -2,7 +2,7 @@
   <header class="relative w-full bg-white">
     <div class="px-4 mx-auto max-w-7xl sm:px-6">
       <div
-        class="flex items-center justify-between py-6 border-b-2 border-gray-100  md:justify-start md:space-x-10"
+        class="flex items-center justify-between py-6 border-b-2 border-gray-100 md:justify-start md:space-x-10"
       >
         <div class="flex justify-start lg:w-0 lg:flex-1">
           <nuxt-link v-if="!onHome" to="/" class="flex items-center">
@@ -29,7 +29,7 @@
         <div id="first" class="-my-2 -mr-2 md:hidden">
           <button
             type="button"
-            class="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md  hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+            class="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
             aria-expanded="false"
             @click="toggleMobileMenu"
           >
@@ -52,7 +52,7 @@
         <div class="items-center justify-end hidden md:flex md:flex-1 lg:w-0">
           <a
             :href="button.link"
-            class="inline-flex items-center justify-center px-4 py-2 ml-8 text-base font-medium text-white bg-black border border-transparent rounded-md shadow-sm  whitespace-nowrap hover:bg-indigo-700"
+            class="inline-flex items-center justify-center px-4 py-2 ml-8 text-base font-medium text-white bg-black border border-transparent rounded-md shadow-sm whitespace-nowrap hover:bg-indigo-700"
           >
             {{ button.title }}
           </a>
@@ -65,11 +65,11 @@
     -->
     <div
       v-if="mobileMenuOpen"
-      class="absolute inset-x-0 top-0 p-2 transition origin-top-right transform  md:hidden"
+      class="absolute inset-x-0 top-0 p-2 transition origin-top-right transform md:hidden"
       style="z-index: 80"
     >
       <div
-        class="bg-white divide-y-2 rounded-lg shadow-lg  ring-1 ring-black ring-opacity-5 divide-gray-50"
+        class="bg-white divide-y-2 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 divide-gray-50"
       >
         <div class="px-5 pt-5 pb-6">
           <div class="flex items-center justify-between">
@@ -87,7 +87,7 @@
             <div class="-mr-2">
               <button
                 type="button"
-                class="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md  hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                class="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
                 @click="closeMobileMenu"
               >
                 <span class="sr-only">Close menu</span>
@@ -102,7 +102,9 @@
           <div class="mt-6 ml-1">
             <nav class="grid gap-y-8" role="navigation">
               <a
-                v-for="navMenuRowItem in mobileNavMenuItems.rowItems"
+                v-for="navMenuRowItem in mobileNavMenuItems.rowItems.filter(
+                  (item) => item.link.split('/')[1] != 'projects'
+                )"
                 :key="navMenuRowItem.title"
                 :href="navMenuRowItem.link"
                 class="flex items-center p-3 -m-3 rounded-md hover:bg-gray-50"
@@ -141,7 +143,7 @@
           <div>
             <a
               :href="button.link"
-              class="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white bg-black border border-transparent rounded-md shadow-sm  hover:bg-indigo-700"
+              class="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white bg-black border border-transparent rounded-md shadow-sm hover:bg-indigo-700"
             >
               {{ button.title }}
             </a>
