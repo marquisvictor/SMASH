6fd2d946fdc40dd44cbf00d242b9411b34cccb82
TaylorOshan <tayoshan@gmail.com>
2023-08-06 18:56:40 -0400

    add page for conf&workshops

---
+++ SMASH/pages/resources/confshops.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <basic-page-template
+      :title="content.title"
+      :body-content="content"
+    >
+    </basic-page-template>
+  </div>
+</template>
+
+<script>
+import BasicPageTemplate from '@/components/basicPageTemplate.vue'
+export default {
+  name: 'PageTitle',
+  components: {
+    BasicPageTemplate,
+  },
+  layout: 'header-footer',
+  data() {
+    return {
+      content: '',
+    }
+  },
+  async fetch() {
+    const content = await this.$content('resources/confshops').fetch()
+    this.content = content
+  },
+}
+</script>
